d1a946001094824e02b06430e3a751d6983a436c
jnst <jnst@outlook.com>
2013-05-22 19:05:39 +0900

    add file utility

---
+++ src/file/FileUtil.java
@@ -0,0 +1,64 @@
+package file;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileUtil {
+
+	public static void main(String[] args) {
+		File f = new File("src/file/data/sample.txt");
+		List<String> lines = readLine(f);
+		List<String> filterdLines = filterNot(lines, "(株)");
+		println(filterdLines);
+	}
+
+	public static List<String> readLine(File f) {
+		List<String> list = new ArrayList<>();
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(f));
+			while (br.ready()) {
+				list.add(br.readLine());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return list;
+	}
+
+	public static List<String> filter(List<String> list, String word) {
+		return filter(list, word, false);
+	}
+
+	public static List<String> filterNot(List<String> list, String word) {
+		return filter(list, word, true);
+	}
+
+	private static List<String> filter(List<String> list, String word, boolean isIgnore) {
+		List<String> filterdList = new ArrayList<>();
+		for (String s : list) {
+			if ((s != null) && !s.isEmpty()) {
+				if (isIgnore && !s.contains(word)) {
+					filterdList.add(s);
+				} else if (!isIgnore && s.contains(word)){
+					filterdList.add(s);
+				}
+			}
+		}
+		return filterdList;
+	}
+
+	public static void println(File f) {
+		println(readLine(f));
+	}
+
+	public static void println(List<String> list) {
+		for (String s : list) {
+			System.out.println(s);
+		}
+	}
+
+}
