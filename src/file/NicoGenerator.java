0f39673118597361b3c19fbc5b8c76d7bbb48539
sato <sato_jun@cyberagent.co.jp>
2013-11-28 12:29:51 +0900

    Fix 1-99 zero fill format

--- src/file/NicoGenerator.java
+++ src/file/NicoGenerator.java
@@ -3,12 +3,12 @@ package file;
 public class NicoGenerator {
 
 	public static void main(String[] args) {
-		System.out.println(createHtml(100, 999));
+		System.out.println(createHtml(1, 999));
 	}
 
 	private static String createHtml(int offset, int limit) {
 		StringBuilder sb = new StringBuilder(limit * 300);
-		sb.append("<html>\n<head>\n<title>niconico</title>\n</head>\n<body>\n");
+		sb.append("<html>\n<head>\n<title>nico</title>\n</head>\n<body>\n");
 		for (int i = offset; i <= limit; i++) {
 			sb.append(createImgTag(i));
 		}
@@ -19,7 +19,7 @@ public class NicoGenerator {
 	private static String createImgTag(int num) {
 		StringBuilder sb = new StringBuilder(300);
 		sb.append("  <img src=\"http://res.nimg.jp/img/base/head/icon/nico/");
-		sb.append(num);
+		sb.append(String.format("%03d", num));
 		sb.append(".gif\" width=\"48\" height=\"48\">");
 		sb.append("\n");
 		return sb.toString();
