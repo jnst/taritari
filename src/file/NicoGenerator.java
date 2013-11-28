fda4c7ad54713fc4e8623af61c2bce7a71609d39
sato <sato_jun@cyberagent.co.jp>
2013-11-28 12:09:00 +0900

    Add niconico left top image generator

---
+++ src/file/NicoGenerator.java
@@ -0,0 +1,28 @@
+package file;
+
+public class NicoGenerator {
+
+	public static void main(String[] args) {
+		System.out.println(createHtml(100, 999));
+	}
+
+	private static String createHtml(int offset, int limit) {
+		StringBuilder sb = new StringBuilder(limit * 300);
+		sb.append("<html>\n<head>\n<title>niconico</title>\n</head>\n<body>\n");
+		for (int i = offset; i <= limit; i++) {
+			sb.append(createImgTag(i));
+		}
+		sb.append("</body>\n</html>\n");
+		return sb.toString();
+	}
+
+	private static String createImgTag(int num) {
+		StringBuilder sb = new StringBuilder(300);
+		sb.append("  <img src=\"http://res.nimg.jp/img/base/head/icon/nico/");
+		sb.append(num);
+		sb.append(".gif\" width=\"48\" height=\"48\">");
+		sb.append("\n");
+		return sb.toString();
+	}
+
+}
